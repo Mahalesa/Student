2abd1eeaa6ef21eda709bb247677c29e82a0beba
Sayali Mahale <mahalesayali1998@gmail.com>
2024-10-19 17:17:34 +0530

    Created App dbcontext and Added migration using entity framework core

---
+++ Student/Student/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Student.Data
+{
+    public class ApplicationDbContext: DbContext
+
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Student.Models.Entities.Student> Students { get; set; }
+
+    }
+}
