73e48532eae7b55dac2812e185e9a6729cd7f76c
Sayali Mahale <mahalesayali1998@gmail.com>
2024-10-20 20:40:30 +0530

    Created ADD method HTTPPOST

--- Student/Student/Controllers/StudentsController.cs
+++ Student/Student/Controllers/StudentsController.cs
@@ -1,13 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Student.Data;
+using Student.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Student.Controllers
 {
     public class StudentsController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Models.Entities.Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+            };
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+            return View();
+
+
         }
     }
 }
---
+++ Student/Student/Models/AddStudentViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Student.Models
+{
+    public class AddStudentViewModel
+    {
+
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool Subscribed { get; set; }
+    }
+}
